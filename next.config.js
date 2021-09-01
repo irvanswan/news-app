ce96c5a0a694dbb2af0fb5f31280a226dea7063f
= <=>
2021-09-01 20:16:02 +0700

    update environtment variable

--- next.config.js
+++ next.config.js
@@ -1,10 +1,21 @@
-module.exports = {
+/* module.exports = {
     env: {
         API_URL: 'http://localhost:3333/news/api',
         API_URL_IMG: 'http://localhost:3333/',
         ORIGIN_URL:'http://localhost:3000',
         SECRET_COOKIE_PASSWORD: 'zj41^B$3yDp@R28VW@akXDQI4BMskEhs*Lx',
         VERSION_STATE:'development',
+        GOOGLE_CLIENT_ID:'681983374499-1son4dkk5f6go6snifmarqsoegfr8il5.apps.googleusercontent.com',
+        FACEBOOK_CLIENT_ID:'576155690033340'
+    },
+} */
+module.exports = {
+    env: {
+        API_URL: 'https://newsapps-server.herokuapp.com/news/api',
+        API_URL_IMG: 'https://newsapps-server.herokuapp.com/',
+        ORIGIN_URL:'https://newsapps-server.herokuapp.com',
+        SECRET_COOKIE_PASSWORD: 'zj41^B$3yDp@R28VW@akXDQI4BMskEhs*Lx',
+        VERSION_STATE:'production',
         GOOGLE_CLIENT_ID:'681983374499-1son4dkk5f6go6snifmarqsoegfr8il5.apps.googleusercontent.com',
         FACEBOOK_CLIENT_ID:'576155690033340'
     },
